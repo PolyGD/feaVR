8294813a4998fd61b43fcef87a0704483a6d9a3f
nathanquinteiro <nathan.quinteiro@gmail.com>
2018-03-17 21:55:21 +0100

    .

---
+++ Assets/ZombieSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieSpawner : MonoBehaviour {
+
+   public GameObject Zombie;
+   float lastSpawnTime;
+   public float spawnTime = 5;
+
+	// Use this for initialization
+	void Start () {
+      lastSpawnTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      if (Time.time > lastSpawnTime + spawnTime) {
+         lastSpawnTime = Time.time;
+         spawnZombie ();
+      }
+	}
+
+   void spawnZombie() {
+      GameObject newZombie = Instantiate (Zombie);
+   }
+}
